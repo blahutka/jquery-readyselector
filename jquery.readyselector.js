8057b9f1370ea70b1423dbd9acc8a847b26e0fde
bp-czech <blahutka@centrum.cz>
2020-05-07 12:05:54 +0200

    fix with container

--- jquery.readyselector.js
+++ jquery.readyselector.js
@@ -1,7 +1,9 @@
 (function ($) {
   var ready = $.fn.ready;
   $.fn.ready = function (fn) {
-    if (this.context === undefined) {
+    var container = $('.action_name.controller_name');
+    
+    if (container.length == 0) {
       // The $().ready(fn) case.
       ready(fn);
     } else if (this.selector) {
